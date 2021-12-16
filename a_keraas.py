aff64e9a2337ce7c9454e713963576836d82846a
janrutger <janrutger@jaruku.nl>
2021-12-16 09:10:34 +0100

    First Karas

--- a_keraas.py
+++ a_keraas.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
-
+from sklearn.model_selection import train_test_split
 
 # load dataset
 data = pandas.read_csv("randomdrive.txt", header=0)
@@ -17,14 +17,14 @@ Y = data['command']
 #print(dataset)
 #X = dataset[:,0:2].astype(float)
 #Y = dataset[:,3]
-
-
 # encode class values as integers
 encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 # convert integers to dummy variables (i.e. one hot encoded)
 dummy_y = np_utils.to_categorical(encoded_Y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, dummy_y, random_state = 0)
 
 
 
@@ -39,8 +39,18 @@ def baseline_model():
 	return model
 
 
-estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=1)
-estimator.fit(X, dummy_y)
+model = baseline_model()
+model.fit(X_train, y_train, epochs=200, batch_size=5, verbose=1)
+
+a, accuracy = model.evaluate(X_test, y_test, verbose=1)
+print(a)
+print (accuracy)
+
+
+# estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=1)
+# estimator.fit(X_train, y_train)
+
+# a, accuracy = estimator.eva
 
 #kfold = KFold(n_splits=10, shuffle=True)
 #results = cross_val_score(estimator, X, dummy_y, cv=kfold)

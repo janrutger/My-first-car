00264952d01cc2a010dd6e72b4f69705e61001d2
janrutger <janrutger@jaruku.nl>
2021-12-25 19:37:11 +0100

    first bot code

--- bot.py
+++ bot.py
@@ -17,6 +17,12 @@ class  Bot:
     def botCoordinates(self):
         return([self.frontLeft, self.frontRight, self.backRight, self.backLeft])
 
+    def botAlign(self, newPosition, position, angle):
+        angle = math.radians(angle)
+        temps = (newPosition[0]-position[0],newPosition[1]-position[1])
+        temps = (temps[0]*math.cos(angle)-temps[1]*math.sin(angle), temps[0]*math.sin(angle)+temps[1]*math.cos(angle))
+        temps = (temps[0]+position[0], temps[1]+position[1])
+        return((int(temps[0]), int(temps[1])))
     
     def botMove(self, move):
         X = 0
@@ -24,35 +30,55 @@ class  Bot:
         speed = 5
 
         if move == "up":
-            self.angle = (self.angle + 0)%360
-            rads = math.radians(self.angle)
+            self.angle_ = (self.angle + 0)%360 #do not change the direction, ony move up
+            rads = math.radians(self.angle_)
             self.frontLeft  = (int(self.frontLeft[X] + speed * math.cos(rads)),  int(self.frontLeft[Y] + speed * math.sin(rads)))
             self.frontRight = (int(self.frontRight[X]+ speed * math.cos(rads)),  int(self.frontRight[Y]+ speed * math.sin(rads)))
             self.backLeft   = (int(self.backLeft[X]  + speed * math.cos(rads)),  int(self.backLeft[Y]  + speed * math.sin(rads)))
             self.backRight  = (int(self.backRight[X] + speed * math.cos(rads)),  int(self.backRight[Y] + speed * math.sin(rads)))
              
         if move == "right":
-            self.angle = (self.angle + 30)%360
+            angle = 30
+            self.angle = (self.angle + angle)%360
             rads = math.radians(self.angle)
-            self.frontLeft  = (int(self.frontLeft[X] + speed * math.cos(rads)),  int(self.frontLeft[Y] + speed * math.sin(rads)))
-            self.frontRight = (int(self.frontRight[X]+ speed * math.cos(rads)),  int(self.frontRight[Y]+ speed * math.sin(rads)))
-            self.backLeft   = (int(self.backLeft[X]  + speed * math.cos(rads)),  int(self.backLeft[Y]  + speed * math.sin(rads)))
-            self.backRight  = (int(self.backRight[X] + speed * math.cos(rads)),  int(self.backRight[Y] + speed * math.sin(rads)))
+
+            new_frontLeft   = (self.frontLeft[X] + speed * math.cos(rads),  self.frontLeft[Y] + speed * math.sin(rads))
+            new_frontRight  = (self.frontRight[X]+ speed * math.cos(rads),  self.frontRight[Y]+ speed * math.sin(rads))
+            new_backLeft    = (self.backLeft[X]  + speed * math.cos(rads),  self.backLeft[Y]  + speed * math.sin(rads))
+            new_backRight   = (self.backRight[X] + speed * math.cos(rads),  self.backRight[Y] + speed * math.sin(rads))
+
+            self.frontleft  = self.botAlign(new_frontLeft, self.frontLeft, angle)
+            self.frontRight = self.botAlign(new_frontRight, self.frontRight, angle)
+            self.backLeft   = self.botAlign(new_backLeft, self.backLeft, angle)
+            self.backRight  = self.botAlign(new_backRight, self.backRight, angle)
+
+            
 
         if move == "down":
-            self.angle = (self.angle + 180)%360
-            rads = math.radians(self.angle)
+            self.angle_ = (self.angle + 180)%360 #do not change the direction, ony move down
+            rads = math.radians(self.angle_)
             self.frontLeft  = (int(self.frontLeft[X] + speed * math.cos(rads)),  int(self.frontLeft[Y] + speed * math.sin(rads)))
             self.frontRight = (int(self.frontRight[X]+ speed * math.cos(rads)),  int(self.frontRight[Y]+ speed * math.sin(rads)))
             self.backLeft   = (int(self.backLeft[X]  + speed * math.cos(rads)),  int(self.backLeft[Y]  + speed * math.sin(rads)))
             self.backRight  = (int(self.backRight[X] + speed * math.cos(rads)),  int(self.backRight[Y] + speed * math.sin(rads)))
 
         if move == "left":
-            self.angle = (self.angle + 330)%360
+            angle = 330
+            self.angle = (self.angle + angle)%360
             rads = math.radians(self.angle)
-            self.frontLeft  = (int(self.frontLeft[X] + speed * math.cos(rads)),  int(self.frontLeft[Y] + speed * math.sin(rads)))
-            self.frontRight = (int(self.frontRight[X]+ speed * math.cos(rads)),  int(self.frontRight[Y]+ speed * math.sin(rads)))
-            self.backLeft   = (int(self.backLeft[X]  + speed * math.cos(rads)),  int(self.backLeft[Y]  + speed * math.sin(rads)))
-            self.backRight  = (int(self.backRight[X] + speed * math.cos(rads)),  int(self.backRight[Y] + speed * math.sin(rads)))  
+
+            new_frontLeft  = (self.frontLeft[X] + speed * math.cos(rads),  self.frontLeft[Y] + speed * math.sin(rads))
+            self.frontleft = self.botAlign(new_frontLeft, self.frontLeft, angle)
+            new_frontRight = (self.frontRight[X]+ speed * math.cos(rads),  self.frontRight[Y]+ speed * math.sin(rads))
+            self.frontRight = self.botAlign(new_frontRight, self.frontRight, angle)
+            new_backLeft   = (int(self.backLeft[X]  + speed * math.cos(rads)),  int(self.backLeft[Y]  + speed * math.sin(rads)))
+            self.backLeft = self.botAlign(new_backLeft, self.backLeft, angle)
+            new_backRight  = (int(self.backRight[X] + speed * math.cos(rads)),  int(self.backRight[Y] + speed * math.sin(rads)))
+            self.backRight = self.botAlign(new_backRight, self.backRight, angle)
+
+            # self.frontLeft  = (int(self.frontLeft[X] + speed * math.cos(rads)),  int(self.frontLeft[Y] + speed * math.sin(rads)))
+            # self.frontRight = (int(self.frontRight[X]+ speed * math.cos(rads)),  int(self.frontRight[Y]+ speed * math.sin(rads)))
+            # self.backLeft   = (int(self.backLeft[X]  + speed * math.cos(rads)),  int(self.backLeft[Y]  + speed * math.sin(rads)))
+            # self.backRight  = (int(self.backRight[X] + speed * math.cos(rads)),  int(self.backRight[Y] + speed * math.sin(rads)))  
         
         print(self.angle)
--- playground.py
+++ playground.py
@@ -27,13 +27,15 @@ while run:
 
     
     Coordinates = car.botCoordinates() # 0=frontleft, 1=frontright
-    #print(Coordinates)
+    print(Coordinates)
     
     
     win.fill((0,0,0))  # Fills the screen with black
     pygame.draw.polygon(win, (255,0,255), Coordinates, 0) #draw de car
     pygame.draw.circle(win, (255,0,0), Coordinates[0], 3, 0)
     pygame.draw.circle(win, (0,255,0), Coordinates[1], 3, 0)
+    pygame.draw.circle(win, (0,0,255), Coordinates[2], 3, 0)
+    pygame.draw.circle(win, (100,100,100), Coordinates[3], 3, 0)
     pygame.display.update() #updte screen to show the car
     
 pygame.quit()

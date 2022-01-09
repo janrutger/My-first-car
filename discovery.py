da58bafa55b42dc151ba3ad1a87add5daee9f5de
janrutger <janrutger@jaruku.nl>
2022-01-09 19:29:42 +0100

    discover MAP

--- discovery.py
+++ discovery.py
@@ -8,40 +8,53 @@ WHITE = (255, 255, 255)
 GREY = (128, 128, 128)
 
 sceneMap = utils.makeScene(None)
-print(sceneMap)
 
 HEIGHT = 800
 WIDTH  = 800
 width  = 20
 
+listToDo = []
+listDone = []
+
 win = pygame.display.set_mode((HEIGHT,WIDTH))
 pygame.display.set_caption("Map discovery")
 
 grid = utils.makeGrid(HEIGHT, WIDTH, width)
-utils.showGrid(win, grid, width)
+sceneLocation = (100,150)
+position, color = utils.scanInit(sceneMap, sceneLocation, width)
+grid[position[0]][position[1]].color = color
+grid[position[0]][position[1]].spotStatus = "open"
+listToDo.append(position)
+
+
 
 run = True
 while run:
-    pygame.time.delay(10)
+    pygame.time.delay(300)
 
     manualCommand = None
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_8:
-                manualCommand = "forward"
-            if event.key == pygame.K_6:
-                manualCommand = "right"
-            if event.key == pygame.K_2:
-                manualCommand = "back"
-            if event.key == pygame.K_4:
-                manualCommand = "left"
+    
+    if len(listToDo) > 0:
+        thisSpot = listToDo[0]
+        listToDo.pop(0)
+        print(thisSpot)
+        neighbors = utils.findNeigbors(thisSpot, sceneMap, width)
+        neighborSpots = utils.updateNeigbors(grid, thisSpot, neighbors)
+        for neighborSpot in neighborSpots:
+            if neighborSpot not in listToDo and neighborSpot not in listDone:
+                listToDo.append(neighborSpot)          
+        print(neighbors)
+        listDone.append(thisSpot)
+    else:
+        print("Finished")
 
 
     ##Some more code here
   
-
+    utils.showGrid(win, grid, width)
     pygame.display.update() #update screen
 
 pygame.quit()
--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (128, 128, 128)
 
+X=0
+Y=1
+
 class Spot:
     def __init__(self, row, col, width):
         self.width =  width
@@ -39,27 +42,109 @@ def showGrid(win, grid, width):
         for j in range(len(grid[i])):
             pygame.draw.line(win, WHITE, (j*width, 0), (j*width, len(grid[i])*width ))
 
-    
-            
+
+def scanInit(sceneMap, location, width):
+    row = location[X] // width
+    col = location[Y] // width
+    white = sceneMap[location] == WHITE
+    if white.all():
+        color = WHITE
+    else:
+        print("ERROR: starting point non white")
+        color = GREY
+
+    return((row, col), color)
+
+
+def findNeigbors(thisSpot, sceneMap, width):
+    result = []
+    up    = (thisSpot[X]*width, thisSpot[Y]*width-width)
+    right = (thisSpot[X]*width+width, thisSpot[Y]*width)
+    down  = (thisSpot[X]*width, thisSpot[Y]*width+width)
+    left  = (thisSpot[X]*width-width, thisSpot[Y]*width)
+
+    print(up, right, down, left)
+
+    for direction in [up, right, down, left]:
+        white = sceneMap[direction] == WHITE
+        black = sceneMap[direction] == BLACK
+
+
+        if white.all():
+            result.append("open")
+        if black.all():
+            result.append("blocked")
+
+    return(result)
+
+def updateNeigbors(grid, thisSpot, neighbors):
+    result = []
+    thisSpot=grid[thisSpot[X]][thisSpot[Y]]
+    print(thisSpot)
+    if neighbors[0] == "open": #UP
+        grid[thisSpot.row][thisSpot.col-1].color = WHITE
+        grid[thisSpot.row][thisSpot.col-1].spotStatus = "open"
+        grid[thisSpot.row][thisSpot.col-1].neighbors.append((thisSpot.row, thisSpot.col-1))
+        result.append((thisSpot.row, thisSpot.col-1))
+    if neighbors[0] == "blocked": #UP
+        grid[thisSpot.row][thisSpot.col-1].color = BLACK
+        grid[thisSpot.row][thisSpot.col-1].spotStatus = "blocked"
+
+    if neighbors[1] == "open": #RIGHT
+        grid[thisSpot.row+1][thisSpot.col].color = WHITE
+        grid[thisSpot.row+1][thisSpot.col].spotStatus = "open"
+        grid[thisSpot.row][thisSpot.col-1].neighbors.append((thisSpot.row+1, thisSpot.col))
+        result.append((thisSpot.row+1, thisSpot.col))
+    if neighbors[1] == "blocked": #RIGHT
+        grid[thisSpot.row+1][thisSpot.col].color = BLACK
+        grid[thisSpot.row+1][thisSpot.col].spotStatus = "blocked"
+
+    if neighbors[2] == "open": #DOWN
+        grid[thisSpot.row][thisSpot.col+1].color = WHITE
+        grid[thisSpot.row][thisSpot.col+1].spotStatus = "open"
+        grid[thisSpot.row][thisSpot.col-1].neighbors.append((thisSpot.row, thisSpot.col+1))
+        result.append((thisSpot.row, thisSpot.col+1))
+    if neighbors[2] == "blocked": #DOWN
+        grid[thisSpot.row][thisSpot.col+1].color = BLACK
+        grid[thisSpot.row][thisSpot.col+1].spotStatus = "blocked"
+
+    if neighbors[3] == "open": #LEFT
+        grid[thisSpot.row-1][thisSpot.col].color = WHITE
+        grid[thisSpot.row-1][thisSpot.col].spotStatus = "open"
+        grid[thisSpot.row][thisSpot.col-1].neighbors.append((thisSpot.row-1, thisSpot.col))
+        result.append((thisSpot.row-1, thisSpot.col))
+    if neighbors[3] == "blocked": #LEFT
+        grid[thisSpot.row-1][thisSpot.col].color = BLACK
+        grid[thisSpot.row-1][thisSpot.col].spotStatus = "blocked"
+    return(result)    
 
 def makeScene(fileName):
     pygame.init()
     win = pygame.display.set_mode((640,480))
     pygame.display.set_caption("Create scenemap")
 
-    if path.isfile("images/filename.png"):
-        image = pygame.image.load("images/filename.png")
-    else:
-        pygame.draw.rect(win, (255,255,255), (20,20,600,440), 0)
-        pygame.draw.rect(win, (0,0,0), (80,125,40,100), 0)
-        pygame.draw.rect(win, (0,0,0), (150,300,40,100), 0)
-        pygame.draw.rect(win, (0,0,0), (430,120,30,100), 0)
-        pygame.draw.rect(win, (0,0,0), (300,0,50,150), 0)
-        pygame.draw.rect(win, (0,0,0), (380,375,125,30), 0)
-        pygame.draw.rect(win, (0,0,0), (320, 250, 40,40), 0)
-        pygame.draw.rect(win, (0,0,0), (180, 200, 40,40), 0)
-        pygame.draw.rect(win, (0,0,0), (500, 300, 20,20), 0)
-        pygame.draw.rect(win, (0,0,0), (500, 150, 20,20), 0)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    sceneMap = pygame.surfarray.array3d(win)
+        if path.isfile("images/filename.png"):
+            image = pygame.image.load("images/filename.png")
+        else:
+            pygame.draw.rect(win, (255,255,255), (40,40,560,400), 0)
+            # pygame.draw.rect(win, (0,0,0), (80,125,40,100), 0)
+            # pygame.draw.rect(win, (0,0,0), (150,300,40,100), 0)
+            # pygame.draw.rect(win, (0,0,0), (430,120,30,100), 0)
+            # pygame.draw.rect(win, (0,0,0), (300,0,50,150), 0)
+            # pygame.draw.rect(win, (0,0,0), (380,375,125,30), 0)
+            # pygame.draw.rect(win, (0,0,0), (320, 250, 40,40), 0)
+            # pygame.draw.rect(win, (0,0,0), (180, 200, 40,40), 0)
+            # pygame.draw.rect(win, (0,0,0), (500, 300, 20,20), 0)
+            # pygame.draw.rect(win, (0,0,0), (500, 150, 20,20), 0)
+
+        sceneMap = pygame.surfarray.array3d(win)
+        pygame.display.update() #update screen
+    pygame.quit()
+
     return(sceneMap)
